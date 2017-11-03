2bcf2e56db0e7cef868ebd2a68c4896d9a2214b4
Nishant George Agrwal <nishantagrwal12993@gmail.com>
2017-11-03 19:17:22 +0530

    Moved models to separate file

--- src/index.ts
+++ src/index.ts
@@ -4,42 +4,13 @@ import Application from './lib/Application'
 import {Model, ModelInstance, ModelManager, SQLString, SQLForeignKey} from './lib/db'
 
 import {mainRouter} from './routes'
+import {Person} from './models'
 
-const Animal = {
-    fields: {
-        collarID: new SQLString()
-    },
-    methods: {
-
-    }
-}
-
-
-const Person = {
-    
-    fields: {
-        name: new SQLString({nullable: true}),
-        pet: new SQLForeignKey({to: Animal})
-    },
-    
-    self: {
-        child: {nullable: true}
-    },
-
-    methods: {
-        getName(): string {
-            const self = this as ModelInstance<typeof Person>
-            return self.name
-        }
-    }
-    
-}
 
 let PersonManager = new ModelManager(Person)
 let p = PersonManager.create({name: 'Nishant'})
+p.name = 'Abhishek'
 PersonManager.save(p)
-
-p.child.child.child
 
 const app = new Application(mainRouter)
 app.start(8080)
--- src/models.ts
+++ src/models.ts
@@ -1,10 +1,31 @@
-import {Model} from './lib/db'
+import {Model, SQLString, SQLForeignKey, ModelInstance} from './lib/db'
 
-export const Person: Model = {
+
+export const Animal = {
     fields: {
-
+        collarID: new SQLString()
     },
     methods: {
 
     }
+}
+
+export const Person = {
+    
+    fields: {
+        name: new SQLString({nullable: true}),
+        pet: new SQLForeignKey({to: Animal})
+    },
+    
+    self: {
+        child: {nullable: true}
+    },
+
+    methods: {
+        getName(): string {
+            const self = this as ModelInstance<typeof Person>
+            return self.name
+        }
+    }
+    
 }
